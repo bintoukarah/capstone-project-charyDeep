0e15bbf573255c573dad4bcd5a31125695fa80d7
bkaramo <bintoukaramoko3@gmail.com>
2022-07-23 00:14:04 +0200

    Finish Addevents style and formatting

--- client/src/pages/AddEvents.js
+++ client/src/pages/AddEvents.js
@@ -1,19 +1,30 @@
 import "../style/index.scss";
-// import logo from "../assets/img/CharyDeep_logo.png"
+import logo from "../assets/img/logo_color_charyDEEp.png";
 
 export default function AddEvents() {
   return (
     <div className="events-container">
-      <div className="events-header">{/* <img src={logo} alt="Logo" /> */}</div>
+      <div className="events-header">
+        <img className="img-logo" src={logo} alt="Logo" />
+      </div>
       <div className="event-form-container">
         <h1>Créer votre évènement</h1>
         <form>
-          <input type="text" placeholder="Titre de l'événement"></input>
-          <label className="img-form-container">
-            <input type="file" name="file" className="form-control-file" />
-            <button type="submit" className="img_btn">submit</button>
+          <label className="form-container" for="mytitle">
+          Le titre
+          <input type="text"></input>
           </label>
-          <input type="text" placeholder="Description"></input>
+          <label className="form-container" for="myfile">
+            <p>Votre image</p>
+            <input type="file" name="file" className="form-control-file" />
+          </label>
+          <button type="submit" className="img_btn">
+           Enregistrer
+          </button>
+          <label className="form-container">
+            La description 
+          <input type="text"></input>
+          </label>
           <button
             className="form-btn"
             type="submit"
